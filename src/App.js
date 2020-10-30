144a86e9f98ff04ac5b9518d4cac4725d1ad2d08
=VelourV <=>
2020-10-29 21:50:17 -0600

    Pantalla Inicial login + Header

--- src/App.js
+++ src/App.js
@@ -2,18 +2,42 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 
+//ReactBS
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // local packages
 import Register from './register';
 import Login from "./login";
 import Stream from "./stream";
 import WatchStream from './watchStream';
+import LogoSmall from './LogoSmall.png'
+
 
 //stylesheets
 import './App.css';
 
+function getLink()
+{
+    return window.location.href.split("/",4)[3]
+}
+
 const App = () => {
     return (
         <div>
+            {getLink() !== "login" &&
+            <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home"><img src ={LogoSmall} alt ="CoolerClass" width = "50%"/></Navbar.Brand>
+            <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                {getLink() !== "" && <Nav.Link href="\">Inicio</Nav.Link>}
+                <Nav.Link href="\login">Login</Nav.Link>
+                 
+                {getLink() !== "register" && <Nav.Link href="\register">Register</Nav.Link>}
+                </Navbar.Collapse>
+            </Navbar>
+            }
             <BrowserRouter>
             {/* <NavBar /> */}
             <main>
@@ -24,6 +48,13 @@ const App = () => {
                     <Route path="/watch-stream" component={WatchStream} />
                 </Switch>
             </main>
+            {getLink() === "" &&
+                <div>
+                    {
+                    // Acá es solo la página de inicio, no sé si vamos a poner algo lmao
+                    }
+                </div>
+            }
             </BrowserRouter>
         </div>
     );
--- src/login.js
+++ src/login.js
@@ -1,6 +1,11 @@
 import React, {useState} from "react";
 import {useHistory} from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Logo from './Logo.png'
 const Login = () => {
     const history = useHistory();
     const initialLoginData = Object.freeze({
@@ -48,21 +53,46 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="email"
-                name="email"
-                placeholder="email@example.com"
-                onChange={handleChange}
-            /> <br/>
-            <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={handleChange}
-            /> <br/>
-            <button>Submit</button>
-        </form>
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+        <Card border='secondary' style={{ width: '30rem',height:'35rem'}} className="text-center">
+        <Card.Header><a href="/"><img alt="Logo" src= {Logo} width="30%"/></a></Card.Header>
+        <Card.Body>
+            <br/>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control onChange = {handleChange} type="email" placeholder="email@example.com"/>
+                </Form.Group>
+                <br/>
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control onChange = {handleChange} type="password" placeholder="Password" />
+                </Form.Group>
+            </Form>
+            <br/><br/><br/>
+            <Card.Link style={{margin:"80px"}} href="/register">Register</Card.Link>
+            <Button style={{margin:"70px"}} variant="primary" type="submit">Submit</Button>
+        </Card.Body>
+        
+        {/*
+            <Form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="email@example.com"
+                    onChange={handleChange}
+                /> <br/>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                /> <br/>
+                <Button>Submit</Button>
+            </Form>
+        */}
+        </Card>
+        </div>
     );
 };
 
